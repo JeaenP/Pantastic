dcbbb84505796a837ef072333cc52eb9bed0477f
JeaenP <jpvpvalarezo@gmail.com>
2024-07-15 00:55:46 -0500

    google login

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,34 +1,32 @@
-import React, { useState, useContext, useEffect } from 'react'
-import './LoginPopup.css'
-import { assets } from '../../assets/assets'
-import { StoreContext } from '../../context/StoreContext'
-import axios from "axios"
+import React, { useState, useContext, useEffect } from 'react';
+import './LoginPopup.css';
+import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
+import axios from "axios";
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 
 const LoginPopup = ({ setShowLogin }) => {
-
-    const {url, setToken} = useContext(StoreContext)
-
-
-    const [currState, setCurrState] = useState("Login")
+    const { url, setToken } = useContext(StoreContext);
+    const [currState, setCurrState] = useState("Login");
     const [data, setData] = useState({
         name: "",
         email: "",
         password: ""
-    })
+    });
 
     const onChangeHandler = (event) => {
         const name = event.target.name;
         const value = event.target.value;
-        setData(data=>({...data, [name]:value}))
-    } 
+        setData(data => ({ ...data, [name]: value }));
+    };
 
     const onLogin = async (event) => {
-        event.preventDefault()
+        event.preventDefault();
         let newUrl = url;
-        if (currState==="Login") {
-            newUrl += "/api/user/login"
+        if (currState === "Login") {
+            newUrl += "/api/user/login";
         } else {
-            newUrl += "/api/user/register"
+            newUrl += "/api/user/register";
         }
 
         const response = await axios.post(newUrl, data);
@@ -37,12 +35,33 @@ const LoginPopup = ({ setShowLogin }) => {
             setToken(response.data.token);
             localStorage.setItem("token", response.data.token);
             setShowLogin(false);
-
         } else {
             alert(response.data.message);
         }
-    } 
+    };
 
+    const responseGoogle = async (response) => {
+        try {
+            const googleData = {
+                tokenId: response.credential,
+            };
+
+            const newUrl = `${url}/api/user/googlelogin`;
+
+            const res = await axios.post(newUrl, googleData);
+
+            if (res.data.success) {
+                setToken(res.data.token);
+                localStorage.setItem("token", res.data.token);
+                setShowLogin(false);
+            } else {
+                alert(res.data.message);
+            }
+        } catch (error) {
+            console.error(error);
+            alert("Error en la autenticación con Google");
+        }
+    };
 
     useEffect(() => {
         const getScrollbarWidth = () => {
@@ -50,7 +69,7 @@ const LoginPopup = ({ setShowLogin }) => {
         };
 
         const scrollbarWidth = getScrollbarWidth();
-        
+
         document.body.style.overflow = 'hidden';
         document.body.style.paddingRight = `${scrollbarWidth}px`;
 
@@ -60,43 +79,47 @@ const LoginPopup = ({ setShowLogin }) => {
         };
     }, []);
 
-    
-
     return (
-        <div className='login-popup'>
-            <form onSubmit={onLogin} className="login-popup-container">
-                <div className="login-popup-title">
-                    <h2>{currState}</h2>
-                    <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
-
-                </div>
-                <div className="login-popup-inputs">
-                    {currState === "Login" ? <></> : <input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder="Nombre" required />}
-                    <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder="Correo Electronico" required />
-                    <input name='password' onChange={onChangeHandler} value={data.password} type="password" placeholder="Contraseña" required />
-
-                </div>
-                <button type="submit">{currState === "Sign Up" ? "Unirse" : "Ingresar"}</button>
-                <div className="login-popup-condition">
-
+        <GoogleOAuthProvider clientId="132281784031-msbd2m567iv2bgt54448u6okut62bnp5.apps.googleusercontent.com">
+            <div className='login-popup'>
+                <form onSubmit={onLogin} className="login-popup-container">
+                    <div className="login-popup-title">
+                        <h2>{currState}</h2>
+                        <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
+                    </div>
+                    
+                    <div className="login-popup-inputs">
+                        {currState === "Login" ? <></> : <input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder="Nombre" required />}
+                        <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder="Correo Electronico" required />
+                        <input name='password' onChange={onChangeHandler} value={data.password} type="password" placeholder="Contraseña" required />
+                    </div>
+                    <button type="submit">{currState === "Sign Up" ? "Unirse" : "Ingresar"}</button>
+                    <div className="login-popup-condition">
+                        {currState === "Login"
+                            ? " "
+                            : (
+                                <div style={{ display: 'flex', alignItems: 'center' }}>
+                                    <input type="checkbox" required />
+                                    <p style={{ margin: '0 0 0 10px' }}>Al continuar, acepto los términos de uso & política de privacidad.</p>
+                                </div>
+                            )
+                        }
+                    </div>
+                    <GoogleLogin
+                        onSuccess={responseGoogle}
+                        onError={() => {
+                            alert('Login Failed');
+                        }}
+                        className="google-login-button"
+                    />
                     {currState === "Login"
-                        ? " "
-                        : (
-                            <div style={{ display: 'flex', alignItems: 'center' }}>
-                                <input type="checkbox" required />
-                                <p style={{ margin: '0 0 0 10px' }}>Al continuar, acepto los términos de uso & política de privacidad.</p>
-                            </div>
-                        )
+                        ? <p>Registrarse en Pantastic <span onClick={() => setCurrState("Sign Up")}>Click aqui</span></p>
+                        : <p>¿Ya esta registrado? <span onClick={() => setCurrState("Login")}>Ingresar aqui</span></p>
                     }
-
-                </div>
-                {currState === "Login"
-                    ? <p>Registrarse en Pantastic <span onClick={() => setCurrState("Sign Up")}>Click aqui</span></p>
-                    : <p>¿Ya esta registrado? <span onClick={() => setCurrState("Login")}>Ingresar aqui</span></p>
-                }
-            </form>
-        </div>
+                </form>
+            </div>
+        </GoogleOAuthProvider>
     )
 }
 
-export default LoginPopup
+export default LoginPopup;
